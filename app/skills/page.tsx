ceee202425267b6c0a021697086078df410b73c2
Tyler Fann <ty.fann95@gmail.com>
2025-11-04 10:53:50 -0500

    add in skills content

--- app/skills/page.tsx
+++ app/skills/page.tsx
@@ -1,3 +1,17 @@
-const Skills  = () => <div>skills</div>
+import { SKILLS } from "@/utils/skills";
+const Skills = () => {
+  return (
+    <div>
+      {Object.keys(SKILLS).map((category) => (
+        <div className="m-5">
+          <div key={category}>{category}</div>
+          <ul>
+            {SKILLS[category].map((skill) => <li>{skill}</li>)}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Skills;
---
+++ utils/skills.ts
@@ -0,0 +1,33 @@
+export const SKILLS = {
+    languages: [
+      "HTML5",
+      "CSS3",
+      "JavaScript (ES6+)",
+      "TypeScript"
+    ],
+    libraries: [
+      "React",
+      "Redux",
+      "React Router",
+      "React Query",
+      "Formik",
+      "Axios",
+      "Express",
+      "MUI",
+      "styled-components"
+    ],
+    testing: [
+      "Jest",
+      "React Testing Library"
+    ],
+    other: [
+      "Git",
+      "GitHub",
+      "Node.js",
+      "REST APIs",
+      "GraphQL",
+      "Jira",
+      "Figma",
+      "Agile"
+    ]
+  };
